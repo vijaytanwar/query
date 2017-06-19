e9a26f828423c162cb3c5ba8662ea702e5cd87e2
Vijay Singh <tamarvijay@gmail.com>
2017-06-19 16:17:20 +0530

    Create query.js

---
+++ query.js
@@ -0,0 +1,166 @@
+/**
+* One of the lightest DOM query library, which has number of reuable function, which can be used to manipulate DOM
+*/
+
+var queryConfig = {
+    addEventListener: document.addEventListener,
+    removeEventListener: document.removeEventListener,
+    createEvent: "createEvent" in document,
+    noop: function () { }
+};
+var query = (function (document) {
+    var readyCallbacks = [],
+        callbacks = [];
+    function find(selector, object) {
+        if (object) {
+            return object.elements[0].querySelectorAll(selector);
+        } else {
+            return document.querySelectorAll(selector);
+        }
+    }
+    function lightJsElement(selector) {
+        if (typeof selector == "object") {
+            switch (selector) {
+                case document: {
+                    this.ready = function (callback) {
+                        readyCallbacks.push(callback);
+                    }
+                }
+            }
+            if (selector.length >= 0) {
+                this.elements = selector;
+            } else {
+                this.elements = [selector];
+            }
+        } else if (typeof selector === "string") {
+            this.elements = find(selector);
+        } else {
+            this.elements = selector;
+        }
+    }
+    lightJsElement.prototype = {
+        find: function (selector) {
+            return new lightJsElement(find(selector, this));
+        },
+        first: function () {
+            return new lightJsElement(this.elements[0]);
+        },
+        addClass: function (className) {
+            var length = this.elements.length,
+                elements = this.elements;
+            for (var i = 0; i < length; i++) {
+                if (elements[i].className.indexOf(className) == -1) {
+                    elements[i].className = elements[i].className.length > 0 ? elements[i].className + " " + className : className;
+                }
+            }
+            return this;
+        },
+        removeClass: function (className) {
+            var length = this.elements.length;
+            for (var i = 0; i < length; i++) {
+                if (this.elements[i].className.length) {
+                    this.elements[i].className.replace(className, "");
+                }
+            }
+            return this;
+        },
+        hasClass: function (className) {
+            return this.elements[0].className.indexOf(className) > -1;
+        },
+        append: function (str) {
+            var length = this.elements.length,
+                isObj = typeof str === "string";
+            for (var i = 0; i < length; i++) {
+                this.elements[i].innerHTML = this.elements[i].innerHTML + (isObj ? str : str.elements[0].innerHTML);
+            }
+        },
+        html: function (str) {
+            var length = this.elements.length,
+                isObj = typeof str === "string";
+            for (var i = 0; i < length; i++) {
+                this.elements[i].innerHTML = isObj ? str : str.elements[0].innerHTML;
+            }
+        },
+        on: function (event, callback) {
+            for (var i = 0; i < this.elements.length; i++) {
+                if (queryConfig.addEventListener)
+                    this.elements[i].addEventListener(event, callback);
+                else
+                    this.elements[i].attachEvent("on" + event, callback);
+            }
+        },
+        off: function (event, callback) {
+            for (var i = 0; i < this.elements.length; i++) {
+                if (queryConfig.removeEventListener) {
+                    this.elements[i].removeEventListener(event, callback);
+                }
+                else
+                    this.elements[i].detachEvent("on" + event, callback);
+            }
+        },
+        each: function (callback) {
+            var length = this.elements.length;
+            for (var i = 0; i < length; i++) {
+                callback(this.elements[i], i);
+            }
+        },
+        trigger: function (event, data) {
+            for (var i = 0; i < this.elements.length; i++) {
+                if (queryConfig.createEvent) {
+                    var evt = document.createEvent("HTMLEvents");
+                    evt.initEvent(event, false, true);
+                    this.elements[i].dispatchEvent(evt, data);
+                }
+                else {
+                    this.elements[i].fireEvent("on" + event, data);
+                }
+            }
+        },
+        prop: function (name, value) {
+            if (value) {
+                var length = this.elements.length;
+                for (var i = 0; i < length; i++) {
+                    this.elements[i][name] = value;
+                }
+                return this;
+            } else {
+                return this.elements[0][name];
+            }
+        },
+        attr: function (name, value) {
+            if (value) {
+                var length = this.elements.length;
+                for (var i = 0; i < length; i++) {
+                    this.elements[i].setAttribute(name, value);
+                }
+                return this;
+            } else {
+                return this.elements[0].getAttribute(name);
+            }
+        },
+        data: function (name, value) {
+            return this.attr("data-" + name, value);
+        },
+        css: function (obj, value) {
+            if (typeof obj == "string") {
+                this.each(function (element) {
+                    element.style[obj] = value;
+                });
+            } else {
+                for (var key in obj) {
+                    this.each(function (element) {
+                        element.style[key] = obj[key];
+                    });
+                }
+            }
+        }
+    }
+    document.addEventListener("DOMContentLoaded", function () {
+        for (var i = 0; i < readyCallbacks.length; i++) {
+            readyCallbacks[i].call();
+        }
+    });
+    return function (selector) {
+        return new lightJsElement(selector);
+    };
+}(document));
